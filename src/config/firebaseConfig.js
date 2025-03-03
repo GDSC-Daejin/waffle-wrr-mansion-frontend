f7fe5df7f55cc4ba97fc525b7f22c47e309b7bb5
alicehr1n63 <clotilda4116@gmail.com>
2025-03-03 21:35:20 +0900

    update firebaseConfig.js

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,27 +1,19 @@
-// Firebase SDK에서 필요한 기능 가져오기
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";  // 인증 추가
-import { getFirestore } from "firebase/firestore";  // Firestore 추가
-import { getAnalytics } from "firebase/analytics";
-
-// Firebase 설정
+/*firebaseConfig.js */
+import {initializeApp} from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyAiT_t07K5sFChhVLqQzBeL2RXpm-1I5c4",
-  authDomain: "waffle-wrr-mansion.firebaseapp.com",
-  databaseURL: "https://waffle-wrr-mansion-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "waffle-wrr-mansion",
-  storageBucket: "waffle-wrr-mansion.firebasestorage.app",
-  messagingSenderId: "140803942934",
-  appId: "1:140803942934:web:c4c44799f30031a45ca337",
-  measurementId: "G-S6CLC1LMR5"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
-
-// Firebase 초기화
+//Firebase 초기화
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);  // 인증 인스턴스 생성
-const db = getFirestore(app);  // Firestore 인스턴스 생성
-const analytics = getAnalytics(app);  // Analytics 초기화 (선택)
-
-// `auth`와 `db`를 export
-export { auth, db };
-export default app;
+//Firebase 서비스 초기화
+const auth = getAuth(app);
+const db = getFirestore(app);
+export {app, auth, db}
