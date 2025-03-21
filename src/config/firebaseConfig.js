869ce268a82a19ff6442a9e755c231b74fef9ae1
zzang-sera <rijooon@gmail.com>
2025-03-21 16:12:46 +0900

    firebase update

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,23 +1,19 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-
+/*firebaseConfig.js */
+import {initializeApp} from "firebase/app";
+import {getAuth} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  storageBucket: process.env.REACT_APP_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
-
-// Firebase 초기화
+//Firebase 초기화
 const app = initializeApp(firebaseConfig);
-
-// Firebase 서비스 초기화
+//Firebase 서비스 초기화
 const auth = getAuth(app);
 const db = getFirestore(app);
-export { app, auth, db };
-
-
+export {app, auth, db}
